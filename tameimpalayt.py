07a65030e93afbf0ce5c0d0a3a9628efa868c59c
Shanmukh Swaroop <shanmukhs99@gmail.com>
2020-07-05 10:55:03 +0530

    Fixed bug when ad was clicked

--- tameimpalayt.py
+++ tameimpalayt.py
@@ -8,9 +8,10 @@ driver.get('https://www.youtube.com/');
 searchBox = driver.find_element_by_xpath('//input[@id="search"]')
 searchBox.send_keys('let it happen tame impala');
 
-driver.implicitly_wait(3)
+
 searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+driver.implicitly_wait(3)
 searchButton.click();
 
-video = driver.find_element_by_xpath('//*[@id="video-title"]')
+video = driver.find_element_by_link_text('Tame Impala - Let It Happen (Official Video)')
 video.click()
